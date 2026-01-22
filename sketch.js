db51d06a3740379272f7edd8ac550f6813ead926
mahallavr <mahallavr2022@gmail.com>
2026-01-22 17:20:32 +0100

    basic start

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let mouseSpeed = 0;
+let prevMouseX = 0;
+let prevMouseY = 0;
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  mouseSpeed = dist(mouseX, mouseY, prevMouseX, prevMouseY);
+
+  let circleSize = map(mouseSpeed, 0, 50, 10, 100);
+
+  fill(100, 150, 255);
+  circle(mouseX, mouseY, circleSize);
+
+  prevMouseX = mouseX;
+  prevMouseY = mouseY;
+}
